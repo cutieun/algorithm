4454fdc8844a806dfecfd1b9090b79e93879a9e7
Jieun Lee <138594200+cutieun@users.noreply.github.com>
2023-08-02 17:17:12 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,15 @@
+function solution(d, budget) {
+    var answer = 0;
+    let arr = d.sort((a, b) => a - b);
+    let sum = 0;
+    
+    for(let i=0; i<d.length; i++){
+        sum += d[i];
+        
+        if(sum <= budget){
+            answer ++;
+        }
+    }
+    
+    return answer;
+}
