068d57146c5ab107042eab942bed8eadb6a016ab
Jieun Lee <138594200+cutieun@users.noreply.github.com>
2023-08-04 10:36:51 +0900

    [level 1] Title: 삼총사, Time: 5.93 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -3,6 +3,7 @@ function solution(number) {
     for (let i = 0; i < number.length; i++){
         for (let j = i+1; j < number.length; j++){
             for (let k = j+1; k < number.length; k++){
+                console.log(number[i], number[j], number[k])
                 if(number[i] + number[j] + number[k] === 0){
                     answer ++;
                 }
