757d4f34506e76e82c6311f744e21cc9cd1b89bf
Jieun Lee <138594200+cutieun@users.noreply.github.com>
2023-08-06 15:17:54 +0900

    [level 1] Title: 시저 암호, Time: 1.50 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,21 +1,21 @@
 function solution(s, n) {
     let answer = '';
     let ascii_arr = [];
+    
     // 대문자 : 65-90
     // 소문자 : 97-122 (+32)
     // space : 32
+    
     for(let i=0; i<s.length; i++){
         let ascii = s.charCodeAt(i);
         let val = ascii + n;
-        // console.log(val);
-        if(ascii === 32){
+        if(ascii === 32){       // 공백
             ascii_arr.push(32);
-        }else if(ascii < 91){
-            ascii_arr.push(val > 90 ? 65+(val-91) : val)
-        } else {
-            ascii_arr.push(val > 122 ? 97+(val-123) : val)
+        } else if(ascii < 91){  // 대문자
+            ascii_arr.push(val > 90 ? (val-26) : val)
+        } else {                // 소문자
+            ascii_arr.push(val > 122 ? (val-26) : val)
         }
     }
-    // console.log(String.fromCodePoint(...ascii_arr));
     return String.fromCodePoint(...ascii_arr);
 }
