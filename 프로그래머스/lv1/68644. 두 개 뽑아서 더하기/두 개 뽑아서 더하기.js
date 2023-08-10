01396510d7e40d1cf7632ea07a048d5faeab2f79
Jieun Lee <138594200+cutieun@users.noreply.github.com>
2023-08-10 15:33:20 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    let mySet = new Set();
+    for(let i=0; i<numbers.length-1; i++){
+        for(let j=i+1; j<numbers.length; j++){
+            mySet.add(numbers[i] + numbers[j]);
+        }
+    }
+    return [...mySet].sort((a, b) => a - b);
+}
