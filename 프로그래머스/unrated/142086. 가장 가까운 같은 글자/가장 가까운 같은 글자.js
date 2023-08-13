1bbfd55593422a49bd9fab19314716c6043bec4c
Jieun Lee <138594200+cutieun@users.noreply.github.com>
2023-08-13 16:14:47 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 3.53 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -2,16 +2,24 @@ function solution(s) {
     let answer = [];
     let idxMap = new Map();
     
-    for(let i=0; i<s.length; i++){
-        if(idxMap.has(s[i])){
-            answer.push(i - idxMap.get(s[i]));
-            idxMap.delete(s[i]);
-            idxMap.set(s[i], i);
+    // for(let i=0; i<s.length; i++){
+    //     if(idxMap.has(s[i])){
+    //         answer.push(i - idxMap.get(s[i]));
+    //         idxMap.set(s[i], i);
+    //     } else {
+    //         answer.push(-1);
+    //         idxMap.set(s[i], i);
+    //     }
+    // }
+    
+    [...s].map((v, i) => {
+        if(idxMap.has(v)){
+            answer.push(i-idxMap.get(v));
         } else {
             answer.push(-1);
-            idxMap.set(s[i], i);
         }
-    }
+        idxMap.set(v, i);
+    })
     
     return answer;
 }
