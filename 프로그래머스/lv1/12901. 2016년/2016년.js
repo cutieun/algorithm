fd7ced47208c88d540a9f4687aa22bd36a89918c
Jieun Lee <138594200+cutieun@users.noreply.github.com>
2023-08-18 17:37:00 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    let month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let day = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU'];
+    let sumDays = b;
+
+    // 총 일수 계산
+    for(let i=0; i<a; i++){
+        sumDays += month[i];
+    }
+    
+    // 요일 계산
+    return day[(sumDays % 7 || day.length) - 1];
+}
