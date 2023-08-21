9c0faf0a925abb57cb3639152f8e1543bee9f6d1
Jieun Lee <138594200+cutieun@users.noreply.github.com>
2023-08-21 22:24:27 +0900

    [unrated] Title: 카드 뭉치, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,17 +1,15 @@
 function solution(cards1, cards2, goal) {
-    let answer = "Yes";
     let idx1 = 0;
     let idx2 = 0;
-    
+
     for(let i=0; i<goal.length; i++){
         if(cards1[idx1] === goal[i]){
             idx1++;
         } else if (cards2[idx2] === goal[i]) {
             idx2++;
         } else {
-            answer = "No";
-            break;
+            return "No"
         }
     }
-    return answer;
+    return "Yes";
 }
