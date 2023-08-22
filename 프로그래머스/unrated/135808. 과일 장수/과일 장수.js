a87f1c9f0852b9eb247ab2170f354f3cf24b2220
Jieun Lee <138594200+cutieun@users.noreply.github.com>
2023-08-22 19:07:27 +0900

    [unrated] Title: 과일 장수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,23 +1,11 @@
 function solution(k, m, score) {
     let answer = 0;
-    let boxes = [];
+    let boxCnt = parseInt(score.length/m);
     
-    // 내림차순 정렬
     score.sort((a, b) => b - a);
     
-    // 사과 박스 나누기
-    for(let i=0; i<score.length; i+=m){
-        let box = [];
-        for(let j=i; j<i+m; j++){
-            box.push(score[j]);
-        }
-        boxes.push(box);
-    }
-    
-    // 계산
-    let boxCnt = parseInt(score.length/m);
-    for(let b=0; b<boxCnt; b++){
-        answer += Math.min(...boxes[b]) * m;
+    for(let i=0; i<boxCnt; i++){
+        answer += score[(m-1)+(m*i)] * m;
     }
     
     return answer;
