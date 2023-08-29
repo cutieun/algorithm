c83d44c0dab7e9a537d2ce09e40ec867cc643819
Jieun Lee <138594200+cutieun@users.noreply.github.com>
2023-08-29 19:11:14 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,22 +1,33 @@
-function solution(N, stages) {
-    var answer = [];
-    let failPercentage = [];
+// function solution(N, stages) {
+//     let answer = [];
+//     let failPercentage = [];
     
-    stages.sort((a, b) => a - b);
+//     stages.sort((a, b) => a - b);
     
-    for(i=0; i<N; i++){
-        let stage = i + 1;
-        let userCnt = stages.filter((v) => v === stage).length;
-        let fail = userCnt/stages.length;
-        for(j=0; j<userCnt; j++){
-            stages.shift();
-        }
-        failPercentage.push([stage, fail]);
-    }
+//     for(i=0; i<N; i++){
+//         let stage = i + 1;
+//         let userCnt = stages.filter((v) => v === stage).length;
+//         let fail = userCnt/stages.length;
+//         for(j=0; j<userCnt; j++){
+//             stages.shift();
+//         }
+//         failPercentage.push([stage, fail]);
+//     }
     
-    failPercentage.sort((a, b) => b[1] - a[1]).forEach(v => {
-        answer.push(v[0]);
-    });
+//     failPercentage.sort((a, b) => b[1] - a[1]).forEach(v => {
+//         answer.push(v[0]);
+//     });
 
-    return answer;
+//     return answer;
+// }
+
+function solution(N, stages) {
+    let answer = [];
+    
+    for(let i=1; i<=N; i++){
+        let unclearUsers = stages.filter((v) => v === i).length;
+        let totalUsers = stages.filter((v) => v >= i).length;
+        answer.push([i, unclearUsers/totalUsers]);
+    }
+    return answer.sort((a, b) => b[1]- a[1]).map((v) => v[0]);
 }
